3b81a02a3523beaf673191115e6e76ab09e20dc4
Nikittua <74291699+Nikittua@users.noreply.github.com>
2022-01-16 23:24:40 +0300

    Solution

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
-#include <iostream>
+#include<iostream>
+#include <map>
+#include <string>
+#include <windows.h>
+#include <clocale>
+#include<vector>
+#include<set>
+#include <iomanip>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    map <string, string> mp;
+    set<string> Tsari;
+    int n; cin >> n;
+    string child;
+    string parent;
+    for (int i = 1; i < n; i++)
+    {
+        cout << i << ") ";
+        cin >> child;
+        cin >> parent;
+        mp[child] = parent;  // добавляем новые элемент
+        Tsari.insert({ parent,child });
+    }
+
+    set<string>::iterator iter = Tsari.begin();
+    map <string, string>::iterator it = mp.begin();
+
+
+    for (it; it != mp.end(); it++)
+    {
+        string current = it->first;
+        int quantity = 0;
+        while( Tsari.count(current) )
+        {
+            current = mp[current];
+            quantity++;
+        }
+        cout << it->first<< " " /*<<"    \t"*/ << quantity - 1 << endl;
+    }
+
+
     return 0;
 }

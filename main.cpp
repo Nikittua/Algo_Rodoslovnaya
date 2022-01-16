bd74b4e2057fc9771a28a5e638c090ed796d5aef
Nikittua <74291699+Nikittua@users.noreply.github.com>
2022-01-17 02:51:55 +0300

    Fixed Solution

--- main.cpp
+++ main.cpp
@@ -10,8 +10,6 @@
 using namespace std;
 
 
-
-
 int main()
 {
     SetConsoleCP(1251);
@@ -22,21 +20,21 @@ int main()
     int n; cin >> n;
     string child;
     string parent;
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i < n; ++i)
     {
         cin >> child;
         cin >> parent;
-        mp[child] = parent;  // добавляем новые элемент
+        mp[child] = parent;
         Tsari.insert({ parent,child });
     }
 
-    set<string>::iterator iter = Tsari.begin();
-    map <string, string>::iterator it = mp.begin();
-    for (it; it != mp.end(); it++)
+    string current;
+    int quantity;
+    for (auto iterator = Tsari.begin(); iterator != Tsari.end(); ++iterator)
     {
-        string current = it->first;
-        int quantity = 0;
-        while( Tsari.count(current) )
+        current = *iterator;
+        quantity = 0;
+        while(mp.count(current))
         {
             current = mp[current];
             quantity++;
